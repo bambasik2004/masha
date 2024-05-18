3250cadc001feb2631d95b41ad1db13f5ec11f23
Bambasik2004 <bambasik2004@icloud.com>
2024-05-18 21:38:27 +0300

    v3 (добавление перевода файлов транслитом, фикс микро штук)

--- move_creatives.py
+++ move_creatives.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 import pandas as pd
+from transliterate import translit
 
 # Название полей таблицы с брендами и тп
-field_name_advertisers = 'ADVERTISERS LIST'
-field_name_brands = 'BRANDS'
-field_name_advertisers_sub_brand = 'SUBBRANDS'
-
+field_name_advertisers = 'Advertisers list'
+field_name_brands = 'Brands list'
+field_name_advertisers_sub_brand = 'Subbrands list'
 
 def main(path_to_dir, excel_path):
     df = pd.read_excel(excel_path)
@@ -22,8 +22,13 @@ def main(path_to_dir, excel_path):
             sub_brands = str(row[field_name_advertisers_sub_brand]).split(';')
             brands += sub_brands
             for brand in brands:
-                if (brand.strip().lower() in creative.lower() or
-                        brand.strip().lower().replace(' ', '_') in creative.lower()):
+                # Переменная brand с замененными пробелами на подчеркивание
+                alt_brand = brand.strip().lower().replace(' ', '_').replace("'", '')
+                # Без замены пробелов
+                brand = brand.strip().lower().replace("'", '')
+                # Перевод транслитом
+                translit_creative = translit(creative.lower(), language_code='ru', reversed=True)
+                if brand in creative.lower() or alt_brand in creative.lower() or brand in translit_creative or alt_brand in translit_creative:
                     if advertiser not in os.listdir(rf'{os.path.dirname(path_to_dir)}/result'):
                         os.makedirs(rf'{os.path.dirname(path_to_dir)}/result/{advertiser}')
                     shutil.move(rf"{path_to_dir}/{creative}", fr'{os.path.dirname(path_to_dir)}/result/{advertiser}')
